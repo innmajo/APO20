8eef260a3eee70fe283b988df091f57fcce9d1f3
innmajo <126289778+innmajo@users.noreply.github.com>
2023-03-22 12:12:02 -0500

    Create Carros.java

---
+++ Carros.java
@@ -0,0 +1,19 @@
+package APO;
+
+public class Carros {
+	
+	//Variables Encapsuladas
+	private String marca;
+	private String color;
+	private int km;
+	
+	//Metodo Set
+	public void SetMarca(String marcaCarro) {
+		this.marca=marcaCarro;
+	}
+	
+	//Metodo Get
+	public String GetMarca() {
+		return marca;
+	}
+}

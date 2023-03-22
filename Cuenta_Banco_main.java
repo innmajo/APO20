ff6ff0371b6b73e87d95b43755dfd71ea6a5cb35
innmajo <126289778+innmajo@users.noreply.github.com>
2023-03-22 12:15:09 -0500

    Create Cuenta_Banco_main.java

---
+++ Cuenta_Banco_main.java
@@ -0,0 +1,20 @@
+package APO;
+
+import javax.swing.JOptionPane;
+
+public class Cuenta_Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Cuenta_Bancaria op=new Cuenta_Bancaria();
+		op.Set_Titular(JOptionPane.showInputDialog("Ingresa el nombre del titular de la cuenta"));
+		op.Set_Cuenta(Integer.parseInt(JOptionPane.showInputDialog("Ingresa el numero de cuenta")));
+		op.Set_Saldo(Integer.parseInt(JOptionPane.showInputDialog("Ingresa el deposito")));
+		System.out.println("Titular: "+op.Get_Titular());
+		System.out.println("Cuenta: "+op.Get_Cuenta());
+		System.out.println("Saldo: "+op.Get_Saldo());
+		
+
+		
+	}
+}

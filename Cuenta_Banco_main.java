b06eed96d2ed246942b31ff86d06797266612fd8
innmajo <126289778+innmajo@users.noreply.github.com>
2023-03-22 12:23:10 -0500

    Update Cuenta_Banco_main.java

--- Cuenta_Banco_main.java
+++ Cuenta_Banco_main.java
@@ -9,12 +9,12 @@ public class Cuenta_Main {
 		Cuenta_Bancaria op=new Cuenta_Bancaria();
 		op.Set_Titular(JOptionPane.showInputDialog("Ingresa el nombre del titular de la cuenta"));
 		op.Set_Cuenta(Integer.parseInt(JOptionPane.showInputDialog("Ingresa el numero de cuenta")));
-		op.Set_Saldo(Integer.parseInt(JOptionPane.showInputDialog("Ingresa el deposito")));
+		op.Set_Saldo(Integer.parseInt(JOptionPane.showInputDialog("Ingresa el saldo")));
+		op.Set_Deposito(Integer.parseInt(JOptionPane.showInputDialog("Ingresa el deposito")));
 		System.out.println("Titular: "+op.Get_Titular());
 		System.out.println("Cuenta: "+op.Get_Cuenta());
-		System.out.println("Saldo: "+op.Get_Saldo());
-		
-
-		
+		System.out.println("Saldo Anterior: "+op.Get_SaldoAnterior());
+		System.out.println("Deposito: "+op.Get_Deposito());
+		System.out.println("Nuevo Saldo: "+op.Get_Saldo());
 	}
 }
